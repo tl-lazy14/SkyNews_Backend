09de99156962fd67bfc8089104297c6fecdcb34a
TungLam1410 <lam2002ttb@gmail.com>
2024-01-02 23:41:12 +0700

    init

---
+++ src/main/java/com/example/skyNews/SkyNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skyNews/SkyNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
